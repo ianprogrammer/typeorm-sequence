d8c8738361509253ebef17d2603c429727d109ae
Ian Oliveira <ian.oliveira@ifood.com.br>
2021-06-10 15:38:36 -0300

    feat: add some support to oracle database

--- src/EntityWithSequence/index.ts
+++ src/EntityWithSequence/index.ts
@@ -1,9 +1,18 @@
 import { getConnection, getManager } from 'typeorm'
 
+
+class DatabaseTypeNotSupported extends Error {
+  constructor() {
+    super("Database type is not supported"); // (1)
+    this.name = "DatabaseTypeNotSupported"; // (2)
+  }
+}
+
 export class EntityWithSequence {
   public async __beforeInsert(): Promise<void> {
     const className = (this as Object).constructor.name
     const entities = getConnection().entityMetadatas
+
     for (const entity of entities) {
       if (entity.name === className) {
         for (const col of entity.columns) {
@@ -11,7 +20,17 @@ export class EntityWithSequence {
           if (metadata.includes('nextVal')) {
             const sequenceName = Reflect.getMetadata('sequenceName', this, col.propertyName)
             const columnName = Reflect.getMetadata('column', this, col.propertyName)
-            const nextVal = await getManager().query(`select nextval('${sequenceName}') as id`)
+            let query = ""
+
+            if (getConnection().options.type == "postgres") {
+              query = `select nextval('${sequenceName}') as id`
+            } else if (getConnection().options.type == "oracle") {
+              query = `SELECT ${sequenceName}.NEXTVAL FROM DUAL;`
+            } else {
+              throw new DatabaseTypeNotSupported()
+            }
+
+            const nextVal = await getManager().query(query)
             this[columnName] = nextVal[0].id
           }
         }
